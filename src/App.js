145e100d1276f668fb63774af15ae02d69ceeac8
Mubashir Reshamwala <mubresham@gmail.com>
2020-02-17 20:14:24 +0530

    useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let [count, countUpdate] = useState(0);
+  useEffect(() => {
+    console.log('Render was executed!');
+    let clickEvent = () => console.log('You clicked the window!');
+    window.addEventListener('click', clickEvent);
+    return () => {
+      window.removeEventListener('click', clickEvent);
+    };
+  // }, [count % 2]); // since count % 2 doesnt change, this useEffect is not called
+  }, []); // When empty array is passed, this useEffect is not called
+  useEffect(() => {
+    console.log('Double trouble!');
+  });// No array passed? Run everytime@
+  // let [obj, setObj] = useState({
+  //   name: 'Mubashir',
+  //   mobile: '123',
+  // });
+  let [obj, setObj] = useState(() => ({
+    name: 'Mubashir',
+    mobile: '123',
+  }));
+  let newMobile = () => {
+    setObj({
+      name: 'Mubashir',
+      mobile: parseInt(Math.random() * 10 ** 10),
+    });
+    countUpdate(prevCount => (prevCount += 2));
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -10,12 +38,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        {obj.name} : {obj.mobile}
+        <br />
+        {count}
+        <button onClick={_ => newMobile()}>Increment</button>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
